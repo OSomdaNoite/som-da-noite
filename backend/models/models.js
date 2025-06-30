5077757002bab5f82a7709473b815dcd7a866934
Minerva <superetro85@gmail.com>
2025-06-29 22:25:17 -0300

    Corrigindo as rotas

--- backend/models/models.js
+++ backend/models/models.js
@@ -1,5 +1,5 @@
 
-const { sequelize } = require('../db');
+const { sequelize } = require('../config/db');
 const { DataTypes } = require('sequelize');
 
 
--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,10 +1,10 @@
+// Configuração Padrão do Express ( AQUI É ONDE FICA A API )
 var express = require('express');
 var router = express.Router();
 const body_parser = require('body-parser')
 const app = express();
 
 
-const Task =  require('./models')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
